efa21d3330afcf1db067fb345f8f947a2b19a224
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-08-10 07:34:53 -0700

    print filename with error handling

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,13 @@ fn main() -> Result<()> {
     println!("Hello, world!");
 
     for entry in read_dir("./")?.filter_map(|e| e.ok()){
+        let entry = entry
+            .path()
+            .to_str()
+            .map(String::from)
+            .ok_or_else( || {
+                Error::Generic(f!("Invalid path {entry:?}"))
+            })?;
         println!("{entry:?}");
     }
 
