2c9726b9026ce9e46f4e7b59c3b1cfe3f990e28f
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-08-10 07:21:14 -0700

    clean start

---
+++ src/error.rs
@@ -0,0 +1,10 @@
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Generic {0}")]
+    Generic(String),
+
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+}
---
+++ src/prelude.rs
@@ -0,0 +1,6 @@
+pub use crate::error::Error;
+pub type Result<T> = core::result::Result<T,Error>;
+
+pub struct W<T>(pub T);
+
+pub use std::format as f;
