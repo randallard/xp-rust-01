d5728aa59aedbc181d463cb597d895a5ceac0131
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-08-14 14:52:02 -0700

    build object with builder

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 #![allow(unused)]
 
-use crate::task::Task;
-use crate::prelude::*;
-use std::fs::read_dir;
+use prelude::*;
+use task::Task;
+use web::{RequestBuilder, Request};
 
 mod error;
 mod prelude;
 mod utils;
 mod task;
+mod web;
 
 fn main() -> Result<()> {
-    let task = Task {
-        done: true,
-        ..Task::new("Task 0223")
-    };
-    println!("{task:#?}");
-
+    let req = RequestBuilder::new()
+        .url("https://some-url.com/task/123")
+        .method("GET")
+        .header("token","user_uuid.exp.sign")
+        .header("some_header_name","some_header_value")
+        .build()?;
+    println!("{req:#?}");
     Ok(())
 }
